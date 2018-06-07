c3e3fd32e2a0d82b4865ee812029f417b7bf0b52
Faiz Mokhtar <mfmokhtar@gmail.com>
2018-06-07 22:02:29 +0800

    Initial Commit

---
+++ toy-static-factory-method/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toy-static-factory-method
+//
+//  Created by Faiz Mokhtar on 07/06/2018.
+//  Copyright © 2018 AirAsia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
